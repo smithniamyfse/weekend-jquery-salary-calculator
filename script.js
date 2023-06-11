601a1d63b75c1258500f31101f037fb16653a0f0
Amy Smith <smithniamyfse@gmail.com>
2023-06-11 14:57:28 -0500

    changed sourcing files, updated numberical values, final check of functionality

--- script.js
+++ script.js
@@ -25,33 +25,39 @@ JQUERY SALARY CALCULATOR AGENDA
 */
 
 // Declare global variable(s)
-    // Variables can be accessed from anywhere in the program
+// Variables can be accessed from anywhere in the program
 
-// Track employee data - Do I need an array to collect all employee data?
+// Collect employee data in an array
+// ? This array may be unnecessary, may original thought was if I deleted the object in the array, it would delete the annualSalary value too but I couldn't make it work. 
 let employeeRoster = [];
 
-// ? Assemble individual employee salary data into a single array
-// ? let compileSalaries = [];
+//  Assemble individual employee salary data into a single array
+let compileSalaries = [];
 
 
 // When the HTML document (DOM) has loaded, call the onReady function
 $(document).ready(onReady);
 
-function onReady(){
-// TEST: does jQuery work?
-// console.log(`jQuery is ready: 🚀`); // TEST: successful
+function onReady() {
+    // TEST: does jQuery work?
+    // console.log(`jQuery is ready: 🚀`); // TEST: successful
 
-// Event Listener for when the submit button is triggered by user
-$('#submit-button').on('click', onSubmitEmployeeData);
+    // Event Listener for when the submit button is triggered by user
+    $('#submit-button').on('click', onSubmitEmployeeData);
+    $('#submit-button').on('click', calculateMonthlyTotal);
+    // ? Curious about the best way to condense and activate different event handlers on one event listener - research says maybe .bind?
+    // ? $('#submit-button').on('click', checkIfNumber);
 
-// Event Listener for when the delete button is triggered by user
-$('#employee-roster-table').on('click', '.delete', onDeleteEmployee);
+
+    // Event Listener for when the delete button is triggered by user
+    $('#employee-roster-table').on('click', '.delete', onDeleteEmployee);
 
 
 } // end onReady
 
+// ** 1. Create an *input form* that captures the following *employee information*: 
 
-// Event handler - input data field
+// Event handler to submitEmployeeData
 function onSubmitEmployeeData(event) {
     // Override and disable default form behavior
     event.preventDefault();
@@ -62,15 +68,90 @@ function onSubmitEmployeeData(event) {
         lastName: $('#last-name').val(),
         idNumber: $('#id-number').val(),
         jobTitle: $('#job-title').val(),
-        annualSalary: $('#annual-salary').val()
+        annualSalary: Number($('#annual-salary').val())
     };
 
     // Add the new employee object to the employeeRoster array
     employeeRoster.push(employee);
+    // TEST: Expect to see if employee objects were being pushed to the array
+    // console.log('Checking employee roster ON ADD: ', employeeRoster); // ✅ Test successful 
 
-    console.log('Checking employee roster: ', employeeRoster);
+    // Add the new annual salary object to the compileSalaries array
+    compileSalaries.push(employee.annualSalary);
+    // TEST: Expect to see if employee objects were being pushed to the array
+    // console.log('Checking compile salary array ON ADD: ', compileSalaries); // ✅ Test successful 
 
-    // Append input values into the employee roster table 
+    // ? Render - I saw these in the lecture notes and didn't fully understand how to rend everything but kept it just in case it is best practices.
+    // Render the posts array
+    render();
+
+} // end onSubmitEmployeeData
+
+// ? Thought was to have the employee.annualSalary value checked if it was a number and then alert if user was entering something other than an integer
+// ? function checkIfNumber() {
+//     var val = $('#annual-salary').val();
+//     var check = $.isNumeric(val);
+//     if (check) {
+//         $('#annual-salary').html("The Value " + "<b>" + val + "</b>"+ " is Numeric");
+//         }
+//         else {
+//         $("#annual-salary").html("The Value " + "<b>" + val + "</b>"+ " is not Numeric");
+//         }
+//         console.log('in Check if number: ' );
+
+// } // end checkIfNumber // ? ❌ Testing and experimenting unsuccessful
+
+// ** 3. Using the stored information (2-b.), calculate *monthly costs* and append this to the DOM.
+// Event Handler: to calculate monthly total and trigger a red-background if monthly sum > 20000
+function calculateMonthlyTotal() {
+    let monthlySum = 0;
+    for (let i = 0; i < compileSalaries.length; i++) {
+        monthlySum += compileSalaries[i];
+        if (monthlySum > 20000) {
+            $('#monthly-total-container').addClass("red-background");
+        }
+    }
+
+    // TEST: Expect Monthly sum to turn red if > 20k and replace previous values working correctly 
+    // console.log('Expect Monthly sum to turn red if > 20k and replace previous values working correctly ', Number(monthlySum)); ✅ Test successful 
+
+    // append sum to monthly-total
+    // convert integer into USD dollar format
+    $('#monthly-total').html(`
+    <div>
+    ${monthlySum.toLocaleString("en-US", { style: "currency", currency: "USD" })}
+    </div>
+    `);
+
+} // end calculateMonthlyTotal
+
+// Event handler deletes employee data
+function onDeleteEmployee() {
+    // Remove employee from table
+    // Hey jQuery, give me the last thing I clicked on
+    // Then give me its parent element 
+    // Then give me its parent element
+    // Then remove the parent element
+    $(this).parent().parent().remove();
+
+    // if ($(this).parent().parent().remove()){
+    //     employeeRoster.pop(employee);
+    //     compileSalaries.pop(employee.annualSalary);} // ❌ Test unsuccessful
+
+    // if ($(this).parent().parent().remove()) {
+    //     employeeRoster.pop(employee);} // ❌ Test unsuccessful
+
+    // TEST: to see if all object properties and values were deleted, the value for employee.annualSalary did not
+    // console.log('Checking employee roster AFTER DELETE: ', employeeRoster); // ❌ Test unsuccessful
+
+} // end onDeleteEmployee
+
+// ** 2. Configure the *'Submit' button* to perform the subsequent actions: 
+// ? Render - I saw these in the lecture notes and didn't fully understand how to rend everything but kept it just in case it is best practices.
+function render() {
+    // Render posts
+    $('#employee-roster-table').empty();
+    for (employee of employeeRoster) {
         $('#employee-roster-table').append(`
             <tr>
                 <td>${employee.firstName}</td>
@@ -78,32 +159,19 @@ function onSubmitEmployeeData(event) {
                 <td>${employee.idNumber}</td>
                 <td>${employee.jobTitle}</td>
                 <td>${employee.annualSalary}</td>
-                <td><button class="delete">Delete</button></td>
+                <td>${employee.annualSalary}</td>
+// ** 4. Create a *delete button* that removes an employee from the DOM.
+                <td><button class="delete">❌ Delete</button></td>
             </tr>
         `);
 
+        // Clear inputs of form 
+        // Hey jQuery find the input elements and set their value to an empty string
+        $('#first-name').val('');
+        $('#last-name').val('');
+        $('#id-number').val('');
+        $('#job-title').val('');
+        $('#annual-salary').val('');
+    }
 
-    // Clear inputs of form 
-    // Hey jQuery find the input elements and set their value to an empty string
-    $('#first-name').val('');
-    $('#last-name').val('');
-    $('#id-number').val('');
-    $('#job-title').val('');
-    $('#annual-salary').val('');
-
-} // end onSubmitEmployeeData
-
-
-function onDeleteEmployee() {
-    // Remove employee from table
-    // Hey jQuery, give me the last thing I clicked on
-        // Then give me its parent element 
-        // Then give me its parent element
-        // Then remove the parent element
-    $(this).parent().parent().remove();
-
-    // Remove employee object from the employeeRoster array
-    employeeRoster.pop(employee);
-} // end onDeleteEmployee
-
-
+} // end render

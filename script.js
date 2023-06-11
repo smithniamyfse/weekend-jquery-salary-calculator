f07e681c6db6d549411488ba12e7cd92a82df9b4
Amy Smith <smithniamyfse@gmail.com>
2023-06-10 22:34:05 -0500

    Commented out required actions

---
+++ script.js
@@ -0,0 +1,42 @@
+/* 
+
+JQUERY SALARY CALCULATOR AGENDA
+    Main Objective: Create an application that records employee salaries 
+    and adds salaries up to report monthly costs.
+        1. Create an *input form* that captures the following *employee information*: 
+            - first name
+            - last name
+            - ID number
+            - job title
+            - annual salary
+
+        2. Configure the *'Submit' button* to perform the subsequent actions: 
+            a. Collect the form information
+            b. Store the information to calculate monthly costs
+            c. Append this information to the DOM 
+            d. Clear the input fields. 
+
+        3. Using the stored information (2-b.), calculate *monthly costs* and append this to the DOM. 
+            - If the *total* monthly cost *exceeds $20,000*, add a *red* BACKGROUND COLOR to the total monthly cost *dollar amount*.
+
+        4. Create a *delete button* that removes an employee from the DOM.
+
+*/
+
+
+// When the HTML document (DOM) has loaded, call the onReady function
+$(document).ready(onReady);
+
+function onReady(){
+// TEST: does jQuery work?
+// console.log(`jQuery is ready: 🚀`); // TEST: successful
+
+
+
+
+
+
+
+
+
+} // end onReady
